37d2cc1a704c8ae1c4d493f97e8944ea60c5e6f2
roshanrjl <roshanrijal700@gmail.com>
2026-08-22 14:38:40 +0545

    fix: correct import path for Navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { initSocket } from "./socketClient/socket";
